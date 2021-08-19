2e6165b6b718bc6cfbca043108dba69b000527c0
Alexander Mina <aquisiesalex@gmail.com>
2021-08-18 20:42:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream
-console.log("Soy app - main")
-=======
 console.log("Soy app - pero estoy en dev - modicado en local cuando en la nube ya fue merged")
->>>>>>> Stashed changes
 
 function suma(a, b){
     return a+b;

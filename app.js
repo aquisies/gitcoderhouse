cd108c5cf0d43f2014b195ed6393f0635cde2481
Alexander Mina <aquisiesalex@gmail.com>
2021-08-18 20:40:22 -0500

    Salve mi trabajo en la rama dev

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 console.log("Soy app - main")
+=======
+console.log("Soy app - pero estoy en dev - modicado en local cuando en la nube ya fue merged")
+>>>>>>> Stashed changes
 
 function suma(a, b){
     return a+b;
